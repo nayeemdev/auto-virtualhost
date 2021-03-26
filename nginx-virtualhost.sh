d14e9f021d5e4af5089a94b5128b74054158d1cc
nayeemdev <nayeem9812@gmail.com>
2021-03-26 17:57:42 +0600

    checked is action is valid

--- nginx-virtualhost.sh
+++ nginx-virtualhost.sh
@@ -17,3 +17,9 @@ if [ "$(whoami)" != 'root' ]; then
 	echo -e $"\nYou dont have permission to run this script please login as root with sudo -s or use sudo.\n"
 		exit 1;
 fi
+
+if [ "$action" != 'create' ] && [ "$action" != 'delete' ] && [ "$action" != 'list' ]
+	then
+		echo -e $"\nPlease Use create or delete or list as action.\n"
+		exit 1;
+fi
